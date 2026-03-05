93f43a58d46319a4df909ad0b4f87ed4e8b4f8e4
Joy <munchinthepool64@gmail.com>
2026-03-05 11:04:53 +0900

    feat: 디자인 컬러 스타일 정의

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  // ──────────────────────────────────────────
+  // Pink Scale
+  // ──────────────────────────────────────────
+
+  static const Color pink50 = Color(0xFFFFFDFE);
+  static const Color pink100 = Color(0xFFFFF7FE);
+  static const Color pink200 = Color(0xFFFDEFFF);
+  static const Color pink300 = Color(0xFFF8D4FF);
+  static const Color pink400 = Color(0xFFF79DFF);
+  static const Color pink500 = Color(0xFFD844CE);
+  static const Color pink600 = Color(0xFFAC08A1);
+
+  // ──────────────────────────────────────────
+  // Pink Transparency
+  // ──────────────────────────────────────────
+
+  static const Color pink60 = Color(0x99FDEFFF);
+
+  // ──────────────────────────────────────────
+  // Gray Scale
+  // ──────────────────────────────────────────
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color gray50 = Color(0xFFFAFAFA);
+  static const Color gray100 = Color(0xFFF4F4F4);
+  static const Color gray200 = Color(0xFFE9E9E9);
+  static const Color gray300 = Color(0xFFD9D9D9);
+  static const Color gray400 = Color(0xFFC4C4C4);
+  static const Color gray500 = Color(0xFF9D9D9D);
+  static const Color gray600 = Color(0xFF7B7B7B);
+  static const Color gray700 = Color(0xFF555555);
+  static const Color gray800 = Color(0xFF434343);
+  static const Color gray900 = Color(0xFF262626);
+  static const Color black = Color(0xFF010101);
+
+  // ──────────────────────────────────────────
+  // Gray Transparency
+  // ──────────────────────────────────────────
+
+  static const Color black70 = Color(0xB3000000);
+
+  // ──────────────────────────────────────────
+  // Special
+  // ──────────────────────────────────────────
+
+  static const Color appleBlack = Color(0xFF111111);
+}
