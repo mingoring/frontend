a2a574a6979ba3a79b4d0dd67b6422a743c2d6e7
Joy <munchinthepool64@gmail.com>
2026-03-05 16:17:24 +0900

    feat: 정적 아이콘 에셋 정의 - arrow, check, bell, book, editpen, setting

--- lib/core/constants/app_icon_assets.dart
+++ lib/core/constants/app_icon_assets.dart
@@ -15,7 +15,30 @@ abstract final class AppIconAssets {
   static const String arrowDown = '$_basePath/ic_arrow_down.svg';
   static const String arrowLeft = '$_basePath/ic_arrow_left.svg';
   static const String arrowRight = '$_basePath/ic_arrow_right.svg';
+  static const String arrowUpPink = '$_basePath/ic_arrow_up_pink.svg';
+  static const String arrowDownPink = '$_basePath/ic_arrow_down_pink.svg';
+
+  // Arrow Direction Mini Icon
+  static const String arrowUpMiniGray = '$_basePath/ic_arrow_up_mini_gray.svg';
+  static const String arrowUpMiniPink = '$_basePath/ic_arrow_up_mini_pink.svg';
+  static const String arrowDownMiniGray = '$_basePath/ic_arrow_down_mini_gray.svg';
+  static const String arrowDownMiniPink = '$_basePath/ic_arrow_down_mini_pink.svg';
+
+  // Check1 Icon
+  static const String check1None = '$_basePath/ic_check1_none.svg';
+  static const String check1True = '$_basePath/ic_check1_true.svg';
+  static const String check1False = '$_basePath/ic_check1_false.svg';
+  static const String check1Plus = '$_basePath/ic_check1_plus.svg';
+
+  // Check2 Icon
+  static const String check2None = '$_basePath/ic_check2_none.svg';
+  static const String check2True = '$_basePath/ic_check2_true.svg';
+  static const String check2False = '$_basePath/ic_check2_false.svg';
 
   // Action Icon
   static const String close = '$_basePath/ic_close.svg';
+  static const String bell = '$_basePath/ic_bell.svg';
+  static const String book = '$_basePath/ic_book.svg';
+  static const String editPen = '$_basePath/ic_editpen.svg';
+  static const String setting = '$_basePath/ic_setting.svg';
 }
